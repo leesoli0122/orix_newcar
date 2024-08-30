1fcc0a6ce45fc4f90d402096583a1f4ee1315884
leesoli0122@gmail.com <leesoli0122@gmail.com>
2024-08-30 15:41:31 +0900

    인풋,버튼,알럿...미완성

--- resources/js/default.js
+++ resources/js/default.js
@@ -39,39 +39,15 @@ $(document).ready(function() {
   });
 
   //툴팁
-	$(".info-tooltip").on('click', function(){
-		var $this = $(this).parents(".tooltip-wrap");
-
-		$this.removeClass('on');
-		if($this.hasClass('on')){
-			$this.removeClass('on');
-		}else{
-			$this.addClass('on');
-		};
-	});
-
-	//툴팁 닫기
+  $(".info-tooltip").on('click', function(){
+    var $this = $(this).parents(".tooltip-wrap");
+    $this.toggleClass('on');
+  });
+  //툴팁 닫기
 	$(".tooltip-close").on('click', function(){
 		var $this = $(this).parents(".tooltip-wrap");
 		$this.removeClass('on');
 	});
-
-  /** 인풋에 value값 있을 때 버튼 활성화하기 **/
-  const inputField = document.querySelector('.inputField');
-  const myButton = document.querySelector('.btn-input');
-
-  // input에 변화가 생길 때마다 실행
-  inputField.addEventListener('input', function() {
-      if (inputField.value.trim() !== "") {
-          // 입력 값이 있으면 버튼을 활성화
-          myButton.classList.add('active');
-          myButton.disabled = false; // 버튼 활성화
-      } else {
-          // 입력 값이 없으면 버튼을 비활성화
-          myButton.classList.remove('active');
-          myButton.disabled = true; // 버튼 비활성화
-      }
-  });
 
   // 페이지 로드 시 초기 상태를 확인
   document.addEventListener('DOMContentLoaded', function() {
